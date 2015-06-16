3a4a530c0dc7cf606a69d662c4f9ee1c26c53170
Henderson <henderson_aryel@hotmail.com>
2015-06-16 14:06:31 -0300

    inserido a classe lista

---
+++ ProjetoJAVA/src/modelo/ListaConta.java
@@ -0,0 +1,20 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaConta {
+	
+	List<Conta> listaConta = new ArrayList<>();
+	
+	public List<Conta> getListaConta() {
+		return listaConta;
+	}
+
+	public void setListaConta(List<Conta> listaConta) {
+		this.listaConta = listaConta;
+	
+	}
+	
+
+}
